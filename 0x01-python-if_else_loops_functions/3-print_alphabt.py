5599d8745c08e0b43d35be335b36283ad938d414
Iyanu22 <olajideiyanu22@gmail.com>
2023-03-19 00:21:18 +0000

    exception

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for number in range(ord('a'), ord('z') + 1):
-    if chr(number) !='q' and chr(number) !='e':
+    if chr(number) != 'q' and chr(number) != 'e':
         print('{:c}'.format(number), end='')
